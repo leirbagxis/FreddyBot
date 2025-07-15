bf204e13335cf35ecb6c1667276d618e0fa421bf
leirbagxis <leirbagxis@gmail.com>
2025-07-15 11:17:15 +0000

    add: dashboard com suporte a .webm

--- internal/api/controllers/separatorController.go
+++ internal/api/controllers/separatorController.go
@@ -24,7 +24,6 @@ func NewSeparatorController(container *container.AppContainer) *SeparatorControl
 
 func (ctrl *SeparatorController) GetSeparator(ctx *gin.Context) {
 	channelIdStr := ctx.Param("channelId")
-
 	channelId, err := strconv.ParseInt(channelIdStr, 10, 54)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -52,30 +51,93 @@ func (ctrl *SeparatorController) GetSeparator(ctx *gin.Context) {
 
 	ext := strings.ToLower(filepath.Ext(stickerData.SeparatorURL))
 
-	var content []byte
 	if ext == ".tgs" {
+		ctx.JSON(http.StatusNotImplemented, gin.H{"error": "Formato TGS ainda não suportado"})
 		return
 	}
 
-	// Content-Type padrão
-	contentType := mime.TypeByExtension(ext)
-	if contentType == "" {
-		contentType = "application/octet-stream"
-	}
-
-	// Faz o download do conteúdo da URL
 	resp, err := http.Get(stickerData.SeparatorURL)
 	if err != nil || resp.StatusCode != http.StatusOK {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar imagem do sticker"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar conteúdo do sticker"})
 		return
 	}
 	defer resp.Body.Close()
 
-	content, err = io.ReadAll(resp.Body)
+	contentType := mime.TypeByExtension(ext)
+	if contentType == "" {
+		if ext == ".webm" {
+			contentType = "video/webm"
+		} else {
+			contentType = "application/octet-stream"
+		}
+	}
+
+	content, err := io.ReadAll(resp.Body)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao ler imagem"})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao ler conteúdo"})
 		return
 	}
 
+	// Adiciona headers explícitos
+	ctx.Header("Content-Type", contentType)
+	ctx.Header("Content-Disposition", "inline; filename=sticker"+ext)
 	ctx.Data(http.StatusOK, contentType, content)
 }
+
+// func (ctrl *SeparatorController) GetSeparator(ctx *gin.Context) {
+// 	channelIdStr := ctx.Param("channelId")
+
+// 	channelId, err := strconv.ParseInt(channelIdStr, 10, 54)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{
+// 			"success": false,
+// 			"message": "ID do canal inválido",
+// 		})
+// 		return
+// 	}
+
+// 	separatorId := ctx.Param("separatorId")
+// 	if separatorId == "" {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Separator ID é obrigatório"})
+// 		return
+// 	}
+
+// 	stickerData, err := ctrl.container.SeparatorRepo.GetSeparatorByTwoID(ctx, channelId, separatorId)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar separator"})
+// 		return
+// 	}
+// 	if stickerData == nil {
+// 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Separator não encontrado"})
+// 		return
+// 	}
+
+// 	ext := strings.ToLower(filepath.Ext(stickerData.SeparatorURL))
+
+// 	var content []byte
+// 	if ext == ".tgs" {
+// 		return
+// 	}
+
+// 	// Content-Type padrão
+// 	contentType := mime.TypeByExtension(ext)
+// 	if contentType == "" {
+// 		contentType = "application/octet-stream"
+// 	}
+
+// 	// Faz o download do conteúdo da URL
+// 	resp, err := http.Get(stickerData.SeparatorURL)
+// 	if err != nil || resp.StatusCode != http.StatusOK {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar imagem do sticker"})
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+
+// 	content, err = io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao ler imagem"})
+// 		return
+// 	}
+
+// 	ctx.Data(http.StatusOK, contentType, content)
+// }
