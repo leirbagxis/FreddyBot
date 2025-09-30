2fb9fc1ac43e73ffe3b25831c6ac9625ee645ff5
leirbagxis <leirbagxis@gmail.com>
2025-09-30 12:49:38 -0300

    refactor: switch from gin.Default() to gin.New() for custom middleware handling

--- internal/api/api.go
+++ internal/api/api.go
@@ -21,7 +21,7 @@ func StartApi(db *gorm.DB, webhookHandler http.Handler) error {
 	defer stop()
 
 	app := container.NewAppContainer(db)
-	router := gin.Default()
+	router := gin.New()
 
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
